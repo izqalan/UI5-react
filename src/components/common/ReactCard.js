0bc79222982022de9e0cd3ad41ef99b829cea253
izqalan <izqalan@gmail.com>
2020-12-04 13:57:23 +0800

    feat: card header click

--- src/components/common/ReactCard.js
+++ src/components/common/ReactCard.js
@@ -3,10 +3,17 @@ import { Card, Text } from "@ui5/webcomponents-react";
 import { spacing } from "@ui5/webcomponents-react-base";
 
 const ReactCard = () => {
+	const handleHeaderClick = () => {
+		alert("header clicked");
+	}
 	return (
 		<div>
-			<Card heading="card">
-				<Text style={spacing.sapUiContentPadding} >Welcome to React.js baby!</Text>
+			<Card heading="card"
+				style={{ width: "300px" }}
+				headerInteractive
+				onHeaderClick={handleHeaderClick}
+			>
+				<Text style={spacing.sapUiContentPadding} >We are on React.js baby!</Text>
 			</Card>
 		</div>
 	)
